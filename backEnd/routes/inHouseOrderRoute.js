35871e3022531c83138a6253b351700a2465bb12
Harry Vu <vunghia200500@gmail.com>
2025-06-17 22:10:23 +0700

    add role control

--- backEnd/routes/inHouseOrderRoute.js
+++ backEnd/routes/inHouseOrderRoute.js
@@ -1,22 +1,22 @@
 import express from 'express'
 import { authenticateToken } from '../middleware/authentication.js';
-
+import { checkAdminAndStaffAccountType } from '../middleware/checkAccountType.js';
 import { getAvailableTable, updateTableStatus, createOrder, addItemsToOrder, getSeatAvailability, 
     getInHouseOrderDetails, getOrderData, getInHouseOrders, updateProductServed, updateProductQuantity, updatePayment } from '../controller/inHouseOrderController.js';
 
 const inHouseOrderRouter = express.Router();
 
-inHouseOrderRouter.post('/getTable', authenticateToken, getAvailableTable);
-inHouseOrderRouter.post('/updateTableStatus', authenticateToken, updateTableStatus)
-inHouseOrderRouter.post('/createOrder', authenticateToken, createOrder)
+inHouseOrderRouter.post('/getTable', authenticateToken, checkAdminAndStaffAccountType, getAvailableTable);
+inHouseOrderRouter.post('/updateTableStatus', authenticateToken, checkAdminAndStaffAccountType, updateTableStatus)
+inHouseOrderRouter.post('/createOrder', authenticateToken, checkAdminAndStaffAccountType, createOrder)
 inHouseOrderRouter.post('/addItems', authenticateToken, addItemsToOrder)
 inHouseOrderRouter.post('/getDetails', authenticateToken, getInHouseOrderDetails)
 inHouseOrderRouter.post('/getOrderData', authenticateToken, getOrderData)
-inHouseOrderRouter.post('/list', authenticateToken, getInHouseOrders)
-inHouseOrderRouter.post('/updateServedQuantity', authenticateToken, updateProductServed)
-inHouseOrderRouter.post('/updateQuantity', authenticateToken, updateProductQuantity)
-inHouseOrderRouter.post('/updatePayment', authenticateToken, updatePayment)
-inHouseOrderRouter.post('/getSeatAvail', authenticateToken, getSeatAvailability)
+inHouseOrderRouter.post('/list', authenticateToken, checkAdminAndStaffAccountType, getInHouseOrders)
+inHouseOrderRouter.post('/updateServedQuantity', authenticateToken, checkAdminAndStaffAccountType, updateProductServed)
+inHouseOrderRouter.post('/updateQuantity', authenticateToken, checkAdminAndStaffAccountType, updateProductQuantity)
+inHouseOrderRouter.post('/updatePayment', authenticateToken, checkAdminAndStaffAccountType, updatePayment)
+inHouseOrderRouter.post('/getSeatAvail', authenticateToken, checkAdminAndStaffAccountType, getSeatAvailability)
 
 
 export default inHouseOrderRouter
